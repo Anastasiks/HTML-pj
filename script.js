0de97ef02456ccc2674beb6a827931cb008b177b
Anastasiks <128732480+Anastasiks@users.noreply.github.com>
2023-11-20 19:57:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
-function func(){
-    document.getElementById("pop_up").style.display = "grid";
-    const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
-  
-    addCSS(".main{ filter: blur(10px); }")
+let add_btn = document.querySelector("#add_btn_menu");
+
+
+
+
+add_btn.addEventListener("click", () => {
+  let new_text_2 = document.querySelector("#pyt_dva").value;
+  document.querySelector("#pyt_dva").value = "";
+
+  if(new_text_2 == 'двоечник'){
+    let new_text = document.querySelector("#add_name_menu").value;
+    document.querySelector("#add_name_menu").value = "";
+    let item_list = document.querySelector("#spisok");
+
+    let new_item = document.createElement("div");
+    new_item.setAttribute("class", "punkt");
+
+    let item_text = document.createElement("h3");
+    item_text.setAttribute("class","punkt_text")
+    item_text.innerHTML = new_text;
+
+    let item_btn = document.createElement("button");
+    item_btn.setAttribute("id", "punkt_btn");
+    item_btn.innerHTML = "Удалить";
+
+    new_item.appendChild(item_text);
+    new_item.appendChild(item_btn);
+    item_list.appendChild(new_item);
+
+    item_btn.addEventListener("click", () => {
+      let parent = item_btn.parentNode;
+      parent.parentNode.removeChild(parent)
+    });
   };
-  function back(){
-    document.getElementById("pop_up").style.display = "none";
-    const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
-  
-    addCSS(".main{ filter: blur(0px); }")
+
+  if(new_text_2 == 'отличник'){
+    let new_text = document.querySelector("#add_name_menu").value;
+    document.querySelector("#add_name_menu").value = "";
+    let item_list = document.querySelector("#spisok_right");
+
+    let new_item = document.createElement("div");
+    new_item.setAttribute("class", "punkt");
+
+    let item_text = document.createElement("h3");
+    item_text.setAttribute("class","punkt_text")
+    item_text.innerHTML = new_text;
+
+    let item_btn = document.createElement("button");
+    item_btn.setAttribute("id", "punkt_btn_right");
+    item_btn.innerHTML = "Удалить";
+
+    new_item.appendChild(item_text);
+    new_item.appendChild(item_btn);
+    item_list.appendChild(new_item);
+
+    item_btn.addEventListener("click", () => {
+      let parent = item_btn.parentNode;
+      parent.parentNode.removeChild(parent)
+    });
   };
-  
-  function ChangeTheme(){
-      var link = document.getElementById("theme-link");
-      let lightTheme = "light.css";
-      let darkTheme = "dark.css";
-    
-  
-      var currTheme = link.getAttribute("href");
-  
-  
-      if(currTheme == lightTheme)
-      {
-        currTheme = darkTheme;
-  
-      }
-      else
-      {    
-        currTheme = lightTheme;
-  
-      }
-  
-      link.setAttribute("href", currTheme);
-  };
+
+
+});
+
+let spisok = document.querySelector("#spisok");
+let spisok_right = document.querySelector("#spisok_right");
+function Zam(){
+  spisok_right.appendChild(spisok.querySelector(".punkt"))
+}
+function Zam_2(){
+  spisok.appendChild(spisok_right.querySelector(".punkt"))
+}
