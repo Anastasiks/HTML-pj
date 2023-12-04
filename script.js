b35f3ca1da93fe0059e2bb63795342a91c1a3797
Anastasiks <128732480+Anastasiks@users.noreply.github.com>
2023-12-05 01:18:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,29 @@
-let add_btn = document.querySelector("#add_btn_menu");
-
-
-
+let add_btn = document.querySelector("#send")
 
 add_btn.addEventListener("click", () => {
-  let new_text_2 = document.querySelector("#pyt_dva").value;
-  document.querySelector("#pyt_dva").value = "";
+  let new_text = document.querySelector("#sms").value;
+  document.querySelector("#sms").value = "";
 
-  if(new_text_2 == 'двоечник'){
-    let new_text = document.querySelector("#add_name_menu").value;
-    document.querySelector("#add_name_menu").value = "";
-    let item_list = document.querySelector("#spisok");
+  let new_t = document.querySelector("#pol").value;
+  document.querySelector("#pol").value = "";
 
-    let new_item = document.createElement("div");
-    new_item.setAttribute("class", "punkt");
+  let item_list = document.querySelector("#block_button");
 
-    let item_text = document.createElement("h3");
-    item_text.setAttribute("class","punkt_text")
-    item_text.innerHTML = new_text;
+  let new_item = document.createElement("div");
+  new_item.setAttribute("class", "button");
 
-    let item_btn = document.createElement("button");
-    item_btn.setAttribute("id", "punkt_btn");
-    item_btn.innerHTML = "Удалить";
+  let item_text = document.createElement("p");
+  item_text.setAttribute("class", "text_1");
+  let item_text_2 = document.createElement("p");
+  item_text_2.setAttribute("class", "text_2");
+  console.log(new_text);
+  item_text_2.innerHTML = new_text;
 
-    new_item.appendChild(item_text);
-    new_item.appendChild(item_btn);
-    item_list.appendChild(new_item);
-
-    item_btn.addEventListener("click", () => {
-      let parent = item_btn.parentNode;
-      parent.parentNode.removeChild(parent)
-    });
-  };
-
-  if(new_text_2 == 'отличник'){
-    let new_text = document.querySelector("#add_name_menu").value;
-    document.querySelector("#add_name_menu").value = "";
-    let item_list = document.querySelector("#spisok_right");
-
-    let new_item = document.createElement("div");
-    new_item.setAttribute("class", "punkt");
-
-    let item_text = document.createElement("h3");
-    item_text.setAttribute("class","punkt_text")
-    item_text.innerHTML = new_text;
-
-    let item_btn = document.createElement("button");
-    item_btn.setAttribute("id", "punkt_btn_right");
-    item_btn.innerHTML = "Удалить";
-
-    new_item.appendChild(item_text);
-    new_item.appendChild(item_btn);
-    item_list.appendChild(new_item);
-
-    item_btn.addEventListener("click", () => {
-      let parent = item_btn.parentNode;
-      parent.parentNode.removeChild(parent)
-    });
-  };
+  console.log(new_t);
+  item_text.innerHTML = new_t;
 
 
-});
-
-let spisok = document.querySelector("#spisok");
-let spisok_right = document.querySelector("#spisok_right");
-function Zam(){
-  spisok_right.appendChild(spisok.querySelector(".punkt"))
-}
-function Zam_2(){
-  spisok.appendChild(spisok_right.querySelector(".punkt"))
-}
+  new_item.appendChild(item_text);
+  new_item.appendChild(item_text_2);
+  item_list.appendChild(new_item);
+})
