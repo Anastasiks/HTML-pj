bdfeeb777823a84ad93bc7d7d0c855cea0f23d4f
Anastasiks <128732480+Anastasiks@users.noreply.github.com>
2024-01-21 00:17:09 +0300

    Add files via upload

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,34 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST',])
+def main():
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        if (request.form.get("login") == "admin") and (request.form.get("parol") == "admin"):
+            return render_template("index3.html")
+        else:
+            return render_template("index2.html")
 
-def registration():
-    if (request.form.get("con_1") != None or request.form.get("con_2") != None) and request.form.get("name") != None:
-        return redirect(url_for("main2"))
-    #print(request.get_data()) #получаем сырые данные
-    print(request.form.get("pos_1")) #получаем имя тега, то что написано
-    #print(request.args.get("pos_@")) # олучаем данные на сервер
-    print(request.form.get("pos_2"))
-    print(request.form.get("name"))
-    return render_template("index.html")
 
-@app.route("/reg", methods=['GET','POST',])
-def main2():
-    if request.form.get("text1") == "8" and request.form.get("text2") == "16" and request.form.get("text3") == "150":
-        return redirect(url_for("good"))
-    elif(request.form.get("text1") != None and request.form.get("text2") != None and request.form.get("text3") != None):
-        return redirect(url_for("lox"))
+@app.route("/admin")
+def admin():
     return render_template("index2.html")
 
-@app.route("/good")
-def good():
+@app.route("/pol")
+def pol():
     return render_template("index3.html")
-
-@app.route("/lox")
-def lox():
-    return render_template("index4.html")
 
 
 if __name__ == "__main__":
